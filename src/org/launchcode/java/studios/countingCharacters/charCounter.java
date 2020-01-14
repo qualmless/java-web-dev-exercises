8842d201eefe41e85b12cb363a9d05e6f916a2dd
qualmless <terasmash@gmail.com>
2020-01-13 20:21:36 -0600

    adding character counter

--- src/org/launchcode/java/studios/countingCharacters/charCounter.java
+++ src/org/launchcode/java/studios/countingCharacters/charCounter.java
@@ -1,5 +1,25 @@
 package org.launchcode.java.studios.countingCharacters;
 
-public class charCounter {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
+public class charCounter {
+    public static void main(String[] args) {
+        String hiddenFigures = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there.";
+        char[] hiddenArray = hiddenFigures.toCharArray();
+//        int hiddenNumber = hiddenArray;
+        Map<Character,Integer> countedChars = new HashMap<>();
+
+//        System.out.println(hiddenArray[8]);
+        for (int i=0; i<hiddenNumber; i++) {
+            System.out.println(i);
+        }
+
+    }
 }
+//        ArrayList<String> students = new ArrayList<>();
+//        ArrayList<Double> grades = new ArrayList<>();
+//        Scanner input = new Scanner(System.in);
+//        String newStudent;
