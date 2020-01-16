a8671cb0793ba568a101352345f21800b76c04bf
qualmless <terasmash@gmail.com>
2020-01-15 18:56:44 -0600

    more exercises in arrayListPractice

--- src/org/launchcode/java/demos/lsn2controlflowandcollections/arrayListPractice.java
+++ src/org/launchcode/java/demos/lsn2controlflowandcollections/arrayListPractice.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 
 public class arrayListPractice {
     public static void main(String[] args) {
+        //array list sum
         ArrayList<Integer> sumInts = new ArrayList<>();
         sumInts.add(1);
         sumInts.add(2);
@@ -24,6 +25,21 @@ public class arrayListPractice {
             }
         }
         System.out.println(sumOfNums);
+//array list word length
+        ArrayList<String> someWords = new ArrayList<>();
+        someWords.add("banana");
+        someWords.add("scarf");
+        someWords.add("hat");
+        someWords.add("prime");
+        someWords.add("actually");
+        someWords.add("black");
+        someWords.add("haunt");
+
+        for (int i = 0; i < someWords.size(); i++) {
+            if (someWords.get(i).length() == 5) {
+                System.out.println(someWords.get(i));
+            }
+        }
 
     }
 }
