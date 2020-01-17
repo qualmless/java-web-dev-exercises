99fe1d2972d27ab59e19bf7616468520f80e2144
qualmless <terasmash@gmail.com>
2020-01-17 13:09:06 -0600

    ArrayPractice completed

--- src/org/launchcode/java/demos/lsn2controlflowandcollections/ArrayPractice.java
+++ src/org/launchcode/java/demos/lsn2controlflowandcollections/ArrayPractice.java
@@ -1,5 +1,6 @@
 package org.launchcode.java.demos.lsn2controlflowandcollections;
 
+import java.awt.*;
 import java.util.Arrays;
 
 public class ArrayPractice {
@@ -17,8 +18,11 @@ public class ArrayPractice {
 
 
         String toDivide = "I would not, could not, in a box. I would not, could not with a fox. I will not eat them in a house. I will not eat them with a mouse.";
-        String[] arrayOfString = {Arrays.toString(toDivide.split(" "))};
+        String[] arrayOfString = toDivide.split(" ");
+        String[] sentenceArray = toDivide.split("\\.");
+//        String[] arrayOfString = new String[Arrays.toString(toDivide.split(" "))];
         System.out.println(Arrays.toString(arrayOfString));
-        System.out.println(Arrays.toString(arrayOfString).length());
+        System.out.println(Arrays.toString(sentenceArray));
+//        System.out.println(Arrays.toString(arrayOfString).length());
     }
 }
