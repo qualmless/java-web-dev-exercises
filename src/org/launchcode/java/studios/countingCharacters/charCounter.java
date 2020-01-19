a40d09e4506a842a829f1dc44a32ab9320038e6b
qualmless <terasmash@gmail.com>
2020-01-18 19:33:50 -0600

    committing studio charCounter

--- src/org/launchcode/java/studios/countingCharacters/charCounter.java
+++ src/org/launchcode/java/studios/countingCharacters/charCounter.java
@@ -6,20 +6,33 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class charCounter {
+    public static final String hiddenFigures = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there.";
+
     public static void main(String[] args) {
-        String hiddenFigures = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there.";
-        char[] hiddenArray = hiddenFigures.toCharArray();
-//        int hiddenNumber = hiddenArray;
-        Map<Character,Integer> countedChars = new HashMap<>();
+        Map<Character, Integer> countedChars = countCharacters(hiddenFigures);
 
-//        System.out.println(hiddenArray[8]);
-//        for (int i=0; i<hiddenNumber; i++) {
-//            System.out.println(i);
-//        }
+        System.out.println("Counting characters!");
+        for (Map.Entry<Character, Integer> countedChar : countedChars.entrySet()) {
+            System.out.println(countedChar.getKey() + ": " + countedChar.getValue());
+        }
+    }
 
+    private static Map<Character, Integer> countCharacters(String inputText) {
+        char[] hiddenArray = inputText.toCharArray();
+        Map<Character, Integer> countedChars = new HashMap<>();
+
+        for (char currentLetter : hiddenArray) {
+            System.out.println(currentLetter);
+            if (Character.isLetter(currentLetter)) {
+                if (countedChars.containsKey(currentLetter)) {
+                    int currentVal = countedChars.get(currentLetter);
+                    countedChars.put(currentLetter, currentVal++);
+                }
+                else {
+                    countedChars.put(currentLetter, 0);
+                }
+            }
+        }
+        return countedChars;
     }
 }
-//        ArrayList<String> students = new ArrayList<>();
-//        ArrayList<Double> grades = new ArrayList<>();
-//        Scanner input = new Scanner(System.in);
-//        String newStudent;
