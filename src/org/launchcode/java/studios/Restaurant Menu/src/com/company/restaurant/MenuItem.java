d384d9ca058fa9f5666113e8c3abd5f9d0d73194
qualmless <terasmash@gmail.com>
2020-01-20 15:10:11 -0600

    restaurant menu classes created and added to

---
+++ src/org/launchcode/java/studios/Restaurant Menu/src/com/company/restaurant/MenuItem.java
@@ -0,0 +1,27 @@
+package com.company.restaurant;
+
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Scanner;
+
+public class MenuItem {
+
+    private String itemName;
+    private Double itemPrice;
+    private String itemDescription;
+    private String category;
+    private Boolean isNew;
+
+    //to construct new menu item
+    public MenuItem(String itemName, Double itemPrice, String itemDescription, String category, Boolean isNew) {
+        this.itemName = itemName;
+        this.itemPrice = itemPrice;
+        this. itemDescription = itemDescription;
+        this.category = category;
+        this.isNew = isNew;
+    }
+
+    //to construct old menu item
+//    public
+}
