0776221d816905431a4c316f669ed158a0ea50ed
qualmless <terasmash@gmail.com>
2020-02-26 17:10:03 -0600

    version control

--- studios/Restaurant Menu/src/com/company/restaurant/Menu.java
+++ studios/Restaurant Menu/src/com/company/restaurant/Menu.java
@@ -1,5 +1,13 @@
 package com.company.restaurant;
 
-public class Menu {
+import java.util.HashMap;
 
+public class Menu {
+//    private void ArrayList() newMenuItem = new ArrayList(String itemName, String itemPrice, String itemDescription, String category, Boolean isNew) {
+//
+//    }
+
+    public void menuItem {
+        HashMap<String, String,String,String,Boolean> students = new HashMap<>();
+    }
 }
